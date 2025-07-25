fde9aa68e46e139f51093c5859ab6f2f937d34c3
ncorreaabondano <ncorreaabondano@roweandclark.org>
2025-07-25 11:48:45 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ELLMaterialAdapter from './components/ELLMaterialAdapter'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <ELLMaterialAdapter />
+    </div>
+  )
+}
+
+export default App
