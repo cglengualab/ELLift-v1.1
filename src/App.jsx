406ba7d1159c743db4045d656f46794d17dc470a
cglengualab <cglengualab@gmail.com>
2025-08-16 07:19:26 -0500

    Update App.jsx Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ELLMaterialAdapter from './components/ELLMaterialAdapter'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <ELLMaterialAdapter />
+      <Analytics />
     </div>
   )
 }
