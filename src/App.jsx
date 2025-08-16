e584d2ba60a5b6ebce1928a3a0ac80a4bc325f4e
cglengualab <cglengualab@gmail.com>
2025-08-16 07:25:51 -0500

    Update App.jsx Error Boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ELLMaterialAdapter from './components/ELLMaterialAdapter'
 import { Analytics } from '@vercel/analytics/react'
+import ErrorBoundary from './components/ErrorBoundary'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <ELLMaterialAdapter />
-      <Analytics />
-    </div>
+    <ErrorBoundary>
+      <div className="min-h-screen bg-gray-50">
+        <ELLMaterialAdapter />
+        <Analytics />
+      </div>
+    </ErrorBoundary>
   )
 }
 
